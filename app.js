d8089fa1048500e22a273f383fa1e0678bca3677
Kayode Atobatele <atobatele@Kayodes-MacBook-Pro.local>
2024-03-09 12:26:01 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('All great feats start with a step'));
+app.get('/', (req, res) => res.send('All great feats start with a step and then another step'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
